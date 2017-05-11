99d4c432361bd4d8c6f2b60a72c11425412738a8
jschimmoeller <jschimmoeller@star2star.com>
2017-05-11 10:01:52 -0400

    update readme

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
+  mainWindow.openDevTools();
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -43,9 +44,10 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit();
+  // if (process.platform !== 'darwin') {
+  //   app.quit()
+  // }
 })
 
 app.on('activate', function () {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var remote = require('electron').remote;
+
+function leave(){
+  console.log('closing')
+  remote.getCurrentWindow().close();
+}
+
+module.exports=leave;
